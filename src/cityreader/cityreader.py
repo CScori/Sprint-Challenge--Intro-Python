27e29694ba390ed677395c29566927aa020fd8ee
CScori <49799016+CScori@users.noreply.github.com>
2020-05-18 20:23:21 -0700

    not sure how but MVP

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,11 +1,15 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
+import csv
 
 class City:
-  def def __init__(self, name, lat, lon):
+  def __init__(self, name, lat, lon):
     self.name = name
     self.lat = lat
     self.lon = lon
+    
+  def __str__(self):
+    return f'The city of {self.name} has a latitude of {self.lat} and a longitude of {self.lon}'
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -20,13 +24,21 @@ class City:
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
 # skip line one and make float of lat n long for test
+ # TODO Implement the functionality to read from the 'cities.csv' file
+  # For each city record, create a new City instance and add it to the 
+  # `cities` list
 
 cities = []
 
 def cityreader(cities=[]):
-  # TODO Implement the functionality to read from the 'cities.csv' file
-  # For each city record, create a new City instance and add it to the 
-  # `cities` list
+    with open('.\cities.csv', 'r') as data:
+      next(data)
+      store = csv.reader(data)
+      
+      for row in store:
+        # cities.append(City(row[city], float(row[lat]), float(row[lng])))
+        cities.append(City(row[0], float(row[3]), float(row[4])))
+      
     
     return cities
 
